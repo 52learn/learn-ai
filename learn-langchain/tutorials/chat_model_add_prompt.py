e9a1179acbed24e2c62dcb29d7cd0009b454ab3b
jinchao <jinchao@vandream.com>
2025-02-18 23:32:35 +0800

    xx

--- learn-langchain/tutorials/chat_model_add_prompt.py
+++ learn-langchain/tutorials/chat_model_add_prompt.py
@@ -1,3 +1,28 @@
-import os
-os.environ["LANGSMITH_TRACING"] = "true"
-os.environ["LANGSMITH_API_KEY"] = "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"
+from .. import lang_smith_config
+
+from langchain.chat_models import init_chat_model
+
+model=init_chat_model("gpt-3.5-turbo",model_provider="openai")
+
+from langchain_core.messages import HumanMessage,SystemMessage
+
+messages=[
+    SystemMessage(content="Translate the following from English into  Chinese."),
+    HumanMessage(content="hi")
+]
+print(model.invoke(messages))
+
+
+for token in model.stream(messages):
+    print(token.content,end="|")
+
+
+
+from langchain.prompts import ChatPromptTemplate
+system_template ="Translate the following from English into  {language}"
+prompt_template=ChatPromptTemplate.from_messages([
+    ("system",system_template),("user","{text}")
+])
+
+prompt = prompt_template.invoke({"language":"Chinese","text":"hi"})
+model.invoke(prompt)
